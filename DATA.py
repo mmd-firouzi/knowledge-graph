79319972c78a900e8be1d5681fa793ddd5ba077f
mohammad firouzi <98333787+mmd-firouzi@users.noreply.github.com>
2023-09-20 00:50:47 +0330

    Create DATA.py

---
+++ DATA.py
@@ -0,0 +1,148 @@
+movieData = [
+    {
+        "name": "Heat",       
+        "year": 1995,
+        "genre": "Crime, Drama, Action",
+        "actors": ["Al Pacino", "Robert De Niro", "Val Kilmer"],
+        "directors": ["Michael Mann"]
+    },
+    {
+        "name": "Silver Linings Playbook",
+        "year": 2012,
+        "genre": "Comedy, Drama, Romance",
+        "actors": ["Bradley Cooper", "Jennifer Lawrence", "Robert De Niro"],
+        "directors": ["David O. Russell"]
+    },
+    {
+        "name": "The Departed",        
+        "year": 2006,
+        "genre": "Crime, Drama, Thriller",
+        "actors": ["Leonardo DiCaprio", "Matt Damon", "Jack Nicholson", "Mark Wahlberg"],
+        "directors": ["Martin Scorsese"]
+    },
+    {
+        "name": "Ocean's Eleven",       
+        "year": 2001,
+        "genre": "Crime, Drama, Thriller",
+        "actors": ["George Clooney", "Brad Pitt", "Matt Damon", "Julia Roberts"],
+        "directors": ["Steven Soderbergh"]
+    },
+    {
+        "name": "The Avengers",       
+        "year": 2012,
+        "genre": "Action, Adventure, Sci-Fi",
+        "actors": ["Robert Downey Jr.", "Chris Evans", "Scarlett Johansson", "Mark Ruffalo"],
+        "directors": ["Joss Whedon"]
+    },
+    {
+        "name": "No Country for Old Men",   
+        "year": 2007,
+        "genre": "Crime, Drama, Thriller",
+        "actors": ["Josh Brolin", "Javier Bardem", "Tommy Lee Jones"],
+        "directors": ["Joel Coen, Ethan Coen"]
+    },
+    {
+        "name": "The Social Network",    
+        "year": 2010,
+        "genre": "Biography, Drama",
+        "actors": ["Jesse Eisenberg", "Andrew Garfield", "Justin Timberlake"],
+        "directors": ["David Fincher"]
+    },
+    {
+        "name": "The Great Gatsby",   
+        "year": 2013,
+        "genre": "Drama, Romance",
+        "actors": ["Leonardo DiCaprio", "Tobey Maguire", "Carey Mulligan"],
+        "directors": ["Baz Luhrmann"]
+    },
+    {
+        "name": "American Hustle",   
+        "year": 2013,
+        "genre": "Crime, Drama",
+        "actors": ["Christian Bale", "Bradley Cooper", "Amy Adams", "Jennifer Lawrence"],
+        "directors": ["David O. Russell"]
+    },
+    {
+        "name": "The Wolf of Wall Street",
+        "year": 2013,
+        "genre": "Biography, Comedy, Crime",
+        "actors": ["Leonardo DiCaprio", "Jonah Hill", "Margot Robbie"],
+        "directors": ["Martin Scorsese"]
+    }
+]
+
+# Create director nodes
+directorData = [
+        "Michael Mann",
+
+        "David O. Russell",
+
+        "Martin Scorsese",
+
+        "Steven Soderbergh",
+
+        "Joss Whedon",
+
+        "Joel Coen, Ethan Coen",
+
+        "David Fincher",
+        
+        "Baz Luhrmann",
+]
+
+actorData = [
+        "Al Pacino",
+        
+        "Robert De Niro",
+        
+        "Val Kilmer",
+        
+        "Bradley Cooper",
+        
+        "Jennifer Lawrence",
+        
+        "Leonardo DiCaprio",
+        
+        "Matt Damon",
+        
+        "Jack Nicholson",
+        
+        "Mark Wahlberg",
+        
+        "George Clooney",
+        
+        "Brad Pitt",
+        
+        "Julia Roberts",
+        
+        "Chris Evans",
+        
+        "Scarlett Johansson",
+        
+        "Mark Ruffalo",
+        
+        "Josh Brolin",
+        
+        "Javier Bardem",
+        
+        "Tommy Lee Jones",
+        
+        "Jesse Eisenberg",
+        
+        "Andrew Garfield",
+        
+        "Justin Timberlake",
+        
+        "Tobey Maguire",
+        
+        "Carey Mulligan",
+        
+        "Amy Adams",
+        
+        "Christian Bale",
+        
+        "Jonah Hill",
+        
+        "Margot Robbie",
+]
+
